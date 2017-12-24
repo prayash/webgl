5a2f456b99d77c5f81ae1b6a33886b14667984ea
Prayash Thapa <prayasht@gmail.com>
2017-12-24 02:41:28 -0700

    Update notes

--- lighting/README.md
+++ lighting/README.md
@@ -1,1 +1,48 @@
 # Lighting
+
+## Flat Shading
+
+WebGL lighting leverages a lot of math under the hood, so it's really helpful to have some basic knowledge of matrix math and model, view, and projection matrices. It's not absolutely necessary, but it's very useful.
+
+Lighting is the process of taking model data and simulating the effects of photons hitting the surface. Generally for real time graphics the approach has been to fake it, rather than trying to accurately recreate the physics of lighting models. Newer approaches can take a more realistic approach, while these lessons deal with more traditional (fake) models of simulating light.
+
+Exercises:
+
+  * Modify the color to play with the basic projected image.
+
+  * Use a varying value in the shader to pass the position down to the fragment shader. Use the position to drive some part of the color, for example the depth.
+
+From a high level, the main script does WebGL work from scratch with the help of some helper libraries. Our program creates the program, the buffers, locations, stores the matrix transforms, and has some internal state for the color. It computes the projection matrix, the view matrix and then invokes the draw loop.
+
+The draw call computes the model matrix and updates all attributes and uniforms and then finally draws the elements, which it draws as `gl.TRIANGLES` with the bunny model that we have. It then fires a `requestAnimationFrame` which then sets off the loop.
+
+What's important here is the shader code. We want to give our model some color as a first step. We accomplish this by changing the `gl_fragColor` variable and giving it any 4-dimensional vector of our choice:
+
+```glsl
+precision mediump float;
+
+// The color of the model
+uniform vec4 color;
+
+void main() {
+  // We can either pass in a color as a uniform
+  // or define a vector from scratch!
+  gl_FragColor = vec4(0.7, 0.3, 1.0, 1.0);
+}
+```
+
+Right now, our model is flat. There's no lighting information there, so there's no way to really differentiate parts of the surface to give our model some sort of shading to make it look like somewhat of a realistic object. This is where normals come in.
+
+
+## Surface Normals
+
+We've gone over how we can set a base color for a model so we can draw it to a screen, but what we really care about is how the surface of the model can behave as if light is shining on it. We can describe the surface of a model by using normals.
+
+Normals are extremely useful for working with lighting. A surface can be defined as a plane at its core. But if just place a point on this plane, we don't really know how that plane is changing over time. What we need to do is create a vector that is of length 1, and point perpendicular to the surface (outwards). This tells us very quickly which direction the surface is pointing in.
+
+![Surface Normals](img/surface-normals.png  "Surface Normals")
+
+In our WebGL program, we've got a buffer for our position of the model, and all the triangles that make up the model.
+
+## Normal Matrix
+
---
+++ lighting/script.js
@@ -0,0 +1,194 @@
+class Sketch {
+  constructor() {
+    console.log(MDN)
+    // Prep the canvas
+    this.canvas = document.getElementById('canvas')
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
+
+    // Grab a context
+    this.gl = MDN.createContext(this.canvas)
+
+    this.webglProgram = this.setupProgram()
+    this.buffers = this.createBuffers()
+    this.locations = this.createLocations()
+    this.transforms = {} // All of the matrix transforms get saved here
+
+    this.color = [1.0, 0.4, 0.7, 1.0]
+
+    //These matrices don't change and only need to be computed once
+    this.computeProjectionMatrix()
+    this.computeViewMatrix()
+    //the model matrix gets re-computed every draw call
+
+    // Start the drawing loop
+    this.draw()
+  }
+
+  createBuffers() {
+    let gl = this.gl
+
+    // See /shared/bunny-model.js for the array buffers referenced by MDN.bunnyModel.positions and MDN.bunnyModel.elements
+
+    let positionsBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, MDN.bunnyModel.positions, gl.STATIC_DRAW)
+
+    let normalsBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, MDN.bunnyModel.vertexNormals, gl.STATIC_DRAW)
+
+    let elementsBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementsBuffer)
+    gl.bufferData(
+      gl.ELEMENT_ARRAY_BUFFER,
+      MDN.bunnyModel.elements,
+      gl.STATIC_DRAW
+    )
+
+    return {
+      positions: positionsBuffer,
+      elements: elementsBuffer,
+      normals: normalsBuffer
+    }
+  }
+
+  setupProgram() {
+    let gl = this.gl
+
+    // Setup a WebGL program
+    let webglProgram = MDN.createWebGLProgramFromIds(
+      gl,
+      'vertex-shader',
+      'fragment-shader'
+    )
+    gl.useProgram(webglProgram)
+
+    // Tell WebGL to test the depth when drawing
+    gl.enable(gl.DEPTH_TEST)
+
+    return webglProgram
+  }
+
+  createLocations() {
+    let gl = this.gl
+
+    let locations = {
+      // Save the uniform locations
+      model: gl.getUniformLocation(this.webglProgram, 'model'),
+      view: gl.getUniformLocation(this.webglProgram, 'view'),
+      projection: gl.getUniformLocation(this.webglProgram, 'projection'),
+      color: gl.getUniformLocation(this.webglProgram, 'color'),
+
+      // Save the attribute location
+      position: gl.getAttribLocation(this.webglProgram, 'position'),
+      normal: gl.getAttribLocation(this.webglProgram, 'normal')
+    }
+
+    return locations
+  }
+
+  computeViewMatrix() {
+    // Move the camera so that the bunny is in view
+    let view = MDN.invertMatrix(MDN.translateMatrix(0, 5, 10))
+
+    //Save as a typed array so that it can be sent to the GPU
+    this.transforms.view = new Float32Array(view)
+  }
+
+  computeProjectionMatrix() {
+    let fieldOfViewInRadians = Math.PI * 0.5
+    let aspectRatio = window.innerWidth / window.innerHeight
+    let nearClippingPlaneDistance = 1
+    let farClippingPlaneDistance = 200
+
+    let projection = MDN.perspectiveMatrix(
+      fieldOfViewInRadians,
+      aspectRatio,
+      nearClippingPlaneDistance,
+      farClippingPlaneDistance
+    )
+
+    // Save as a typed array so that it can be sent to the GPU
+    this.transforms.projection = new Float32Array(projection)
+  }
+
+  computeModelMatrix(now) {
+    // Rotate according to time
+    let model = MDN.rotateYMatrix(now * 0.0005)
+
+    //Save as a typed array so that it can be sent to the GPU
+    this.transforms.model = new Float32Array(model)
+
+    /*
+    Performance caveat: in real production code it's best to re-use
+    objects and arrays. It's best not to create new arrays and objects
+    in a loop. This example chooses code clarity over performance.
+  */
+  }
+
+  computerNormalMatrix() {
+    var modelView = MDN.multiplyMatrices(
+      this.transforms.view,
+      this.transforms.model
+    )
+
+    // Run the function from the shared/matrices.js that takes
+    // the inverse and then transpose of the provided matrix
+    // and returns a 3x3 matrix.
+    this.transforms.normalMatrix = MDN.normalMatrix(modelView)
+  }
+
+  draw() {
+    let gl = this.gl
+    let now = Date.now()
+
+    // Compute our model matrix
+    this.computeModelMatrix(now)
+    this.computerNormalMatrix()
+
+    // Update the data going to the GPU
+    this.updateAttributesAndUniforms()
+
+    // Perform the actual draw
+    gl.drawElements(
+      gl.TRIANGLES,
+      MDN.bunnyModel.elements.length,
+      gl.UNSIGNED_SHORT,
+      0
+    )
+
+    // Run the draw as a loop
+    requestAnimationFrame(this.draw.bind(this))
+  }
+
+  updateAttributesAndUniforms() {
+    let gl = this.gl
+
+    // Set the uniforms
+    gl.uniformMatrix4fv(
+      this.locations.projection,
+      false,
+      this.transforms.projection
+    )
+    gl.uniformMatrix4fv(this.locations.view, false, this.transforms.view)
+    gl.uniformMatrix4fv(this.locations.model, false, this.transforms.model)
+    gl.uniform4fv(this.locations.color, this.color)
+
+    // Set the positions attribute
+    gl.enableVertexAttribArray(this.locations.position)
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.positions)
+    gl.vertexAttribPointer(this.locations.position, 3, gl.FLOAT, false, 0, 0)
+
+    // Set the normals attribute
+    gl.enableVertexAttribArray(this.locations.normal)
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.normals)
+    gl.vertexAttribPointer(this.locations.normal, 3, gl.FLOAT, false, 0, 0)
+
+    // Set the elements array which defines the order the positions will be drawn
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffers.elements)
+  }
+}
+
+let sketch = new Sketch()
+sketch.draw()
